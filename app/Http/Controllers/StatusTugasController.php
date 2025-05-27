c499d1d0b9cbb25bf1570158704fa5581e63dcdd
Rasnomawan <alvinorasya9@gmail.com>
2025-05-27 16:00:04 +0700

    isi mirgration

---
+++ app/Http/Controllers/StatusTugasController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Status_tugas;
+use Illuminate\Http\Request;
+
+class StatusTugasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Status_tugas $status_tugas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Status_tugas $status_tugas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Status_tugas $status_tugas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Status_tugas $status_tugas)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_05_27_084508_create_tugas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tugas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained();
+            $table->string('judul_tugas');
+            $table->string('deskripsi');
+            $table->string('prioritas');
+            $table->string('status_id')->default('berjalan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tugas');
+    }
+};
