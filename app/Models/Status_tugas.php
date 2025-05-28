0ba27ac09ac3558a8bee4a8ac796fe67fd9cb27c
afriansyah234 <113077621+afriansyah234@users.noreply.github.com>
2025-05-28 14:07:22 +0700

    tailwind + daisyui

--- app/Models/Status_tugas.php
+++ app/Models/Status_tugas.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Status_tugas extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama_status'
+    ];
+
+
 }
--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -1,39 +1,40 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h2 class="text-center">Manajemen Project</h2>
-            <div class="text-center">
-               <a class="btn btn-success" href="{{ route('project.create') }}"><i class="bi bi-plus-circle me-2"></i>Tambah project</a> 
-            </div>
-            <table class="table table-bordered shadow mt-4">
-                <thead>
-                    <tr>
-                        <th>Nama Project</th>
-                        <th>Deskripsi</th>
-                        <th> Status</th>
-                        <th>Penanggung Jawab</th>
-                        <th>Aksi prikitiw</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        @foreach ($projects as $project)
-                         <td>{{ $project->nama_project }}</td>
-                        <td>{{ $project->deskripsi }}</td>
-                        <td>{{ $project->status_project }}</td>
-                        <td>{{ $karyawans->Nama_karyawan }}</td>
-                        <td>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="text-center">Manajemen Project</h2>
+                <div class="text-center">
+                    <a class="btn btn-success" href="{{ route('project.create') }}"><i
+                            class="bi bi-plus-circle me-2"></i>Tambah project</a>
+                </div>
+                <table class="table table-bordered shadow mt-4">
+                    <thead>
+                        <tr>
+                            <th>Nama Project</th>
+                            <th>Deskripsi</th>
+                            <th> Status</th>
+                            <th>Penanggung Jawab</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            @foreach ($projects as $project)
+                                <td>{{ $project->nama_project }}</td>
+                                <td>{{ $project->deskripsi }}</td>
+                                <td>{{ $project->status_project }}</td>
+                                <td>{{ $karyawans->Nama_karyawan }}</td>
+                                <td>
 
-                        </td>
-                        @endforeach
-                       
-                    </tr>
-                </tbody>
-            </table>
+                                </td>
+                            @endforeach
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
-    
     </div>
-</div>
 @endsection
