5d8eb87d9f51da21c670656459eba8e21af6811b
Rasnomawan <alvinorasya9@gmail.com>
2025-05-30 15:57:36 +0700

    nih

--- database/migrations/2025_05_30_075904_create_laporans_table.php
+++ database/migrations/2025_05_30_075904_create_laporans_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('file_lampirans', function (Blueprint $table) {
+        Schema::create('laporans', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('file_lampirans');
+        Schema::dropIfExists('laporans');
     }
 };
