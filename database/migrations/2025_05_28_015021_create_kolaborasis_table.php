4d75f1aaadb5c2a924967bd07b5c480a95660fe8
Rasnomawan <alvinorasya9@gmail.com>
2025-05-30 13:22:33 +0700

    kolaborasi view dan migration

--- database/migrations/2025_05_28_015021_create_kolaborasis_table.php
+++ database/migrations/2025_05_28_015021_create_kolaborasis_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('kolaborasis', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('karyawan_id')->constrained();
             $table->timestamps();
         });
     }
--- resources/views/kolaborasi/index.blade.php
+++ resources/views/kolaborasi/index.blade.php
@@ -1,4 +1,43 @@
 @extends('layouts.app')
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="text-center">Manajemen Project</h2>
+            <div class="text-center">
+               <a class="btn btn-success" href="{{ route('project.create') }}"><i class="bi bi-plus-circle me-2"></i>Tambah project</a> 
+            </div>
+            <table class="table table-bordered shadow mt-4">
+                <thead>
+                    <tr>
+                        <th>Nama Proje</th>
+                        <th>Deskripsi</th>
+                        <th>Status</th>
+                        <th>Penanggung Jawab</th>
+                        <th>Aksi prikitiw</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        @foreach ($projects as $project)
+                    <tr>
+                         <td>{{ $project->nama_project }}</td>
+                        <td>{{ $project->deskripsi }}</td>
+                        <td>{{ $project->status_project }}</td>
+                        <td>{{ $project->karyawan->nama_karyawan  ?? '-'}}</td>
+                        <td>
+                            <a class="btn btn-warning" href="{{ route('project.show',$project->id) }}">detail</a>
+                            <form action="{{ route('project.destroy',$project->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">Hapus</button>
+                            </form>
 
+                        </td>
+                       
+                    </tr>
+                        @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
