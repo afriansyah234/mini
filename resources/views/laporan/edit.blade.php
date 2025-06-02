95e0c3a8e6979b7c936315015613dfc41d0c7651
Rasnomawan <alvinorasya9@gmail.com>
2025-06-02 11:33:30 +0700

    lampiran

--- resources/views/laporan/edit.blade.php
+++ resources/views/laporan/edit.blade.php
@@ -65,7 +65,7 @@
                             <select class="form-select @error('statuslaporan') is-invalid @enderror" id="statuslaporan" name="statuslaporan" required>
                                 <option disabled selected>-- Pilih Status --</option>
                                 @foreach($statuses as $status)
-                                    <option value="{{ $status }}" {{ old('statuslaporan', $laporan->statuslaporan) == $status ? 'selected' : '' }}>
+                                    <option value="{{ $status->id }}" {{ old('statuslaporan', $laporan->statuslaporan) == $status->id ? 'selected' : '' }}>
                                         {{ ucfirst($status) }}
                                     </option>
                                 @endforeach
